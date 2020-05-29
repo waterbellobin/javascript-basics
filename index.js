8d1c1e3c4fc5dcdec26708ff4984b6c89ce9b386
waterbellobin <lgjong951@naver.com>
2020-05-29 23:24:59 +0900

    changed file name and added more stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+// console.log('Yo see me in console');
+// // this is a comment
+// /*
+// longer
+// comments
+// */
+// const a = 3;
+// let b = a - 2;
+// const what = "Sujong";
+// console.log(b, a);
+// console.log(what);
+// const bool = true;
+const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+//camel case
+console.log(daysOfWeek[0]);
+
+const sujongInfo = {name: "Sujong", age: "27", language: ["JavaScript", "C++"]};
+console.log(sujongInfo.language);
+sujongInfo.language = "Python";
+console.log(sujongInfo.language);
