e99df21971f0209be55134dbe4eb58f844e8c022
waterbellobin <lgjong951@naver.com>
2020-06-02 22:09:12 +0900

    change color by css code

--- index.js
+++ index.js
@@ -109,14 +109,30 @@
 
 // init();
 
-function handleOffline(){
-    console.log("See you later!");
+// function handleOffline(){
+//     console.log("See you later!");
+// }
+
+// function handleOnline(){
+//     console.log("Welcome!");
+// }
+
+// window.addEventListener("offline", handleOffline);
+// window.addEventListener("online", handleOnline); // checks online/offline status
+
+const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    } else{
+        title.className = "";
+    }
+}
+function init(){
+    title.addEventListener("click", handleClick);
 }
 
-function handleOnline(){
-    console.log("Welcome!");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline); // checks online/offline status
-
+init();
