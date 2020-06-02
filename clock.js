abeab938883a233b0d9d2c389d6fb8202e141520
waterbellobin <lgjong951@naver.com>
2020-06-02 22:44:21 +0900

    making a clock showing the current time

---
+++ clock.js
@@ -0,0 +1,8 @@
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+function init(){
+    
+}
+
+init();
--- index.js
+++ index.js
@@ -120,19 +120,50 @@
 // window.addEventListener("offline", handleOffline);
 // window.addEventListener("online", handleOnline); // checks online/offline status
 
+// const title = document.querySelector("#title");
+// const CLICKED_CLASS = "clicked";
+
+// function handleClick(){
+//     const currentClass = title.className;
+//     if(currentClass !== CLICKED_CLASS){
+//         title.className = CLICKED_CLASS;
+//     } else{
+//         title.className = "";
+//     }
+// }
+// function init(){
+//     title.addEventListener("click", handleClick);
+// }
+
+// init();
+
+// const title = document.querySelector("#title");
+// const CLICKED_CLASS = "clicked";
+
+// function handleClick(){
+//     const hasClass = title.classList.contains(CLICKED_CLASS);
+//     if(!hasClass){
+//         title.classList.add(CLICKED_CLASS);
+//     } else{
+//         title.classList.remove(CLICKED_CLASS);
+//     }
+// }
+// function init(){
+//     title.addEventListener("click", handleClick);
+// }
+
+// init(); // not perfectly understood
+
+// same code below
 const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentClass = title.className;
-    if(currentClass !== CLICKED_CLASS){
-        title.className = CLICKED_CLASS;
-    } else{
-        title.className = "";
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 function init(){
     title.addEventListener("click", handleClick);
 }
 
-init();
+init(); 
+
