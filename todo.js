55149dc8f047a629b5a81aa6775a93b80f79ae25
waterbellobin <lgjong951@naver.com>
2020-06-08 23:02:50 +0900

    taking a 'todo' input and console logging it

--- todo.js
+++ todo.js
@@ -1,9 +1,29 @@
-const toDoForm = document.querySelector(".js-toDoForm");
-    toDoInput = form.querySelector("input");
+const toDoForm = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoForm.querySelector("input"),
     toDoList = document.querySelector(".js-toDoList");
 
+const TODOS_LOCAL = 'toDos';
+
+function paintToDo(text){
+    console.log(text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = ""; // clear input
+}
+
+function loadToDos(){
+    const toDos = localStorage.getItem(TODOS_LOCAL);
+    if (toDos !== null){
+    }
+}
+
 function init(){
-    
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
